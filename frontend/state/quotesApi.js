319403687bb41048a934ec7a17aa6534f3108ac8
Sydney <proteimus@gmail.com>
2024-06-12 21:39:43 -0500

    mutation state

--- frontend/state/quotesApi.js
+++ frontend/state/quotesApi.js
@@ -1,1 +1,20 @@
 // create your RTK Query endpoints here
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+export const quotesApi = createApi({
+  reducerPath: 'quotesApi',
+  endpoints: builder => ({
+    getQuotes: builder.query({
+
+    }),
+    createQuote: builder.mutation({
+
+    }),
+    toggleQuote: builder.mutation({
+
+    }),
+    deleteQuote: builder.mutation({
+      
+    })
+  })
+})
